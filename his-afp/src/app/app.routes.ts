18b19d59af42e85577120285ea275006794e8cad
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-12 11:02:23 +0100

    feat: ✨ update routing for modifica-pz and enhance patient loading
    Refactor the routing for the modifica-pz feature to use a dynamic patient ID in the URL.
    Update the navigation method to construct the URL with the patient ID directly.
    Enhance the modifica-pz component to handle patient data loading with improved error handling
    and loading states, providing a better user experience.

--- his-afp/src/app/app.routes.ts
+++ his-afp/src/app/app.routes.ts
@@ -14,14 +14,13 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/accettazione-pz/accettazione-pz').then((a)=>a.AccettazionePz)
   },
-  {
-    path: 'modifica-pz',
-    // component: ModificaPz,
-    loadComponent: () => import('./features/modifica-pz/modifica-pz').then((m) => m.ModificaPz),
-  },
+  // {
+  //   path: 'modifica-pz',
+  //   loadComponent: () => import('./features/modifica-pz/modifica-pz').then((m) => m.ModificaPz),
+  // },
   {
     // /modifica-pz?id=2
-    path: 'modifica-pz:patientId',
+    path: 'modifica-pz/:patientId',
     loadComponent: () => import('./features/modifica-pz/modifica-pz').then((m) => m.ModificaPz),
   },
   {
